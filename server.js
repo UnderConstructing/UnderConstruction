cb1c61f5e5bd7d1b687668eca77dd358dc78da89
cherry bharati <cherry.bharati@gmail.com>
2020-11-25 16:06:51 -0500

    stash

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const exhbs = require('express-handlebars');
+app.engine("handlebars", exhbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars")
 
 app.listen(PORT, function () {
   console.log("listening at port " + PORT);
