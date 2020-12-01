d969c978dc3e1410166c9f35ed38acf8e042c181
Jordan Morse <jtm6496@gmail.com>
2020-11-30 21:14:02 -0500

    one line of change

---
+++ models/contractorsdb.js
@@ -0,0 +1,19 @@
+module.exports = function (sequelize, DataTypes) {
+    var contractorsdb = sequelize.define("contractorsdb", {
+        id: { 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        contractor_name: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return contractorsdb
+} 
